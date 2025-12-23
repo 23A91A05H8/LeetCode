f4efa51e77b28a3c29cd7c944b58dd8e93afc905
23A91A05H8 <23A91A05H8@aec.edu.in>
2025-12-23 14:59:15 +0530

    Time: 0 ms (100%), Space: 11.1 MB (84.09%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        int totalsum = 0;
-        for(int i=0;i<arr.size();i++){
-            int currentsum = 0;
-            for(int j=i;j<arr.size();j++){
-                currentsum = currentsum+arr[j];
-                int length = j-i+1;
-                if(length%2==1){
-                    totalsum = totalsum+currentsum;
-                }
-            }
+        int n = arr.size();
+        long long res = 0;
+        for (int i = 0; i < n; i++) {
+            long long total = (long long)(i + 1) * (n - i);
+            long long odd = (total + 1) / 2;
+            res += arr[i] * odd;
         }
-        return totalsum;
+        return (int)res;
     }
 };
